d155d2a1598f0f4c5eab587eff242a73361c1801
TimBluesWin <timisutantyo@gmail.com>
2017-11-11 17:43:25 +0700

    Add functions to AirConditionerController
    This commit intends to set up the basic functions for the controller.
    Rifqi will implement the TemperatureSensor class. Currently, the
    function to set the temperature is still very basic, it just sets the
    temperature to a set value, instead of having some calculations to
    determine the appropriate temperature.

--- src/controller/AirConditionerController.java
+++ src/controller/AirConditionerController.java
@@ -1,5 +1,26 @@
 package controller;
 
 public class AirConditionerController {
-
+	TemperatureSensor ts;
+	int temperature;
+	
+	public AirConditionerController(int temperature)
+	{
+		ts = new TemperatureSensor();
+		this.temperature = temperature;
+	}
+	
+	public double setTemperature()
+	{
+		String setting = "";
+		if(ts.getTemperature() > 30)
+		{
+			temperature = 24;
+		}
+		else if(ts.getTemperature() < 21)
+		{
+			temperature = 24;
+		}
+		return temperature;
+	}
 }
