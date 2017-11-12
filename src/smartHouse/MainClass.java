fd1eeff9a4f517bd22ec4fa0f0623d24955c5f8d
TimBluesWin <timisutantyo@gmail.com>
2017-11-12 22:30:07 +0700

    Add some test cases

--- src/smartHouse/MainClass.java
+++ src/smartHouse/MainClass.java
@@ -6,6 +6,8 @@ public class MainClass {
 	public static void main(String[] args)
 	{
 		AirConditionerController acc = new AirConditionerController(24);
-		System.out.println(acc.setTemperature(100));
+		System.out.println("Temperature is now " + acc.setTemperature(100));
+		System.out.println("Temperature is now " + acc.setTemperature(15));
+		System.out.println("Temperature is now " + acc.setTemperature(22));
 	}
 }
