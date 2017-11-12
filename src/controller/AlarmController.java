9ac7e7bc1380a304f1e90dbadef8e1a6e0be97a1
TimBluesWin <timisutantyo@gmail.com>
2017-11-12 22:44:43 +0700

    Now for the AlarmController

--- src/controller/AlarmController.java
+++ src/controller/AlarmController.java
@@ -1,5 +1,28 @@
 package controller;
 
 public class AlarmController {
-
+	boolean alarm = false;
+	
+	public AlarmController(boolean alarm)
+	{
+		this.alarm = alarm;
+	}
+	
+	public boolean setBlinds(double windSpeed)
+	{
+		String settings = "";
+		boolean alarm = false;
+		if(windSpeed > 70)
+		{
+			settings = "Sounding alarm";
+			alarm = true;
+		}
+		else if(windSpeed < 20)
+		{
+			settings = "Turning off alarm";
+			alarm = false;
+		}
+		
+		return alarm;
+	}
 }
