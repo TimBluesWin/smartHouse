361352636410846a60594ca97649ab6fabe90871
TimBluesWin <timisutantyo@gmail.com>
2017-11-12 22:34:39 +0700

    Add functions for BlindsController class

--- src/controller/BlindsController.java
+++ src/controller/BlindsController.java
@@ -1,5 +1,28 @@
 package controller;
 
 public class BlindsController {
-
+	double windSpeed;
+	
+	public BlindsController(double windSpeed)
+	{
+		this.windSpeed = windSpeed;
+	}
+	
+	public boolean setBlinds(double windSpeed)
+	{
+		String settings = "";
+		boolean closedBlinds = false;
+		if(windSpeed > 70)
+		{
+			settings = "Closing blinds for safety";
+			closedBlinds = true;
+		}
+		else if(windSpeed < 20)
+		{
+			settings = "Opening blinds";
+			closedBlinds = false;
+		}
+		
+		return closedBlinds;
+	}
 }
