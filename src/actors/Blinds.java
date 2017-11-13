a365f3a07c5c5a1517e0ed517ea9e4628ea8ebd1
TimBluesWin <timisutantyo@gmail.com>
2017-11-13 09:39:58 +0700

    Add function for Blinds

--- src/actors/Blinds.java
+++ src/actors/Blinds.java
@@ -1,5 +1,18 @@
 package actors;
 
 public class Blinds {
-
+	boolean closed = false;
+	
+	public void closedBlinds(boolean closedBlinds)
+	{
+		closed = closedBlinds;
+		if(closed)
+		{
+			System.out.println("Blinds are closed!");
+		}
+		else
+		{
+			System.out.println("Blinds are opened!");
+		}
+	}
 }
