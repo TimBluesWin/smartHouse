b16aeda10fcdb93b3aa586722315335a1f5563f5
TimBluesWin <timisutantyo@gmail.com>
2017-11-13 09:36:09 +0700

    Add function for Alarm

--- src/actors/Alarm.java
+++ src/actors/Alarm.java
@@ -1,5 +1,18 @@
 package actors;
 
 public class Alarm {
-
+	boolean active = false;
+	
+	public void setAlarm(boolean isActive)
+	{
+		active = isActive;
+		if(active)
+		{
+			System.out.println("Beep! Beep!");
+		}
+		else
+		{
+			System.out.println("...........");
+		}
+	}
 }
